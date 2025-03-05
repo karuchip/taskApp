177676dc15e5f34a9f8e1748eb2ff0591d42f746
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2025-03-05 15:53:15 +0900

    [Add] セッション情報保持機能を追加

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const session = require('express-session');
 const app = express()
 const port = 3000
 
 //フォーム送信を受け取るためのミドルウェア設定
 app.use(express.json()); //JSONリクエストを解析
 app.use(express.urlencoded({ extended: true })); //フォームデータを解析
+
+//セッション設定
+app.use(session({
+  secret: 'my_secret_key', //セッションの秘密鍵
+  resave: false,
+  savaUninitialized: false,
+  cookie: { maxAge: 60000 } //セッションの有効期限 60秒
+}))
 
 //静的ファイルのルーティング
 const path = require('path')
@@ -23,21 +32,26 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
   res.render('register');
 })
+//ログイン後のページ
 app.get('/tasks', (req, res) => {
-  res.render('tasks');
+  if (!req.session.user) {
+    return res.status(401).send("ログインしてください");
+  }
+  res.render('tasks', { userName: req.session.user.userName});
 })
 
+//仮のユーザーデータ
 const users = {
   "userName": "hanako",
   "email": "test@test",
   "password": "password"
 }
 
+//ログイン処理
 app.post('/tasks', (req, res) => {
   const {email, password} = req.body;
-  console.log(email);
-  console.log(password);
   if(email === users.email && password === users.password) {
+    req.session.user = users;
     console.log("[承認完了]メールアドレスとパスワードが一致");
     res.redirect("/tasks");
   }else {
