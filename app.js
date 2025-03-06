4898f8096d192a807ce93adff2fc8ead511ca0f0
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2025-03-06 17:20:36 +0900

    [Add] タスク追加機能のapiを定義、ユーザー認証処理のコメントアウト

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use('/static', express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 
+
 app.get('/', (req, res) => {
   res.render('index')
 })
@@ -61,25 +62,25 @@ app.get('/logout', (req, res) => {
 })
 
 //ログイン処理
-app.post('/tasks', (req, res) => {
-  const {email, password} = req.body;
-  const user = users.find(u => u.email ===email && u.password === password);
-  if(user) {
-    req.session.user = user;
-    console.log("[承認完了]メールアドレスとパスワードが一致");
-    res.redirect("/tasks");
-  }else {
-    res.status(401).send("承認失敗：メールアドレスまたはパスワードが違います");
-  }
-})
+// app.post('/tasks', (req, res) => {
+//   const {email, password} = req.body;
+//   const user = users.find(u => u.email ===email && u.password === password);
+//   if(user) {
+//     req.session.user = user;
+//     console.log("[承認完了]メールアドレスとパスワードが一致");
+//     res.redirect("/tasks");
+//   }else {
+//     res.status(401).send("承認失敗：メールアドレスまたはパスワードが違います");
+//   }
+// })
 
 //ログイン後のページ遷移
-app.get('/tasks', (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).send("ログインしてください");
-  }
-  res.render('tasks', { userName: req.session.user.userName});
-})
+// app.get('/tasks', (req, res) => {
+//   if (!req.session.user) {
+//     return res.status(401).send("ログインしてください");
+//   }
+//   res.render('tasks', { userName: req.session.user.userName});
+// })
 
 //新規ユーザー登録API
 app.post('/register', (req, res) => {
@@ -92,7 +93,27 @@ app.post('/register', (req, res) => {
   users.push({userName, email, password});
   req.session.user = {userName,email};
   res.redirect("/tasks");
+})
 
+//(開発用)一時的に認証をスキップ
+app.get("/tasks", async (req, res) => {
+  res.render('tasks');
+});
+
+//add画面への遷移
+app.get('/add', (req, res) => {
+  res.render('add');
+})
+
+//仮のtaskデータ
+const tasksData = [];
+
+//タスク追加API
+app.post('/add', (req, res) => {
+  const {addTaskName, addProjectName, addDueDate, addDescription, addPriority} = req.body;
+  tasksData.push({addTaskName, addProjectName, addDueDate, addDescription, addPriority});
+  console.log(tasksData);
+  res.redirect("/tasks");
 })
 
 
